64ca1f24762a6ea79ad1cd90d73664e08e23c345
Faisal Arshed <faisal.arshed.28@gmail.com>
2020-08-17 18:15:38 +0500

    Remove redundant code

--- src/components/AddButton.js
+++ src/components/AddButton.js
@@ -2,14 +2,13 @@ import React from 'react';
 import { View, TouchableWithoutFeedback } from 'react-native';
 import RNFileSelector from 'react-native-file-selector';
 import { useNavigation } from '@react-navigation/native';
-// import { WebView } from 'react-native-webview';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 import { getStoragePermission, checkStoragePermissions } from '../utils/permissions';
 import Icon from './Icon';
 import { primaryColor } from '../constants';
 
-function AddButton(props) {
+function AddButton() {
 	const navigation = useNavigation();
 
 	async function selectFile() {
@@ -17,6 +16,7 @@ function AddButton(props) {
 		if (!granted) await getStoragePermission();
 		RNFileSelector.Show({
 			title: 'Select epub file',
+			filter: '.*\\.(epub|EPUB)$',
 			onDone: (path) => {
 				navigation.navigate('reader', { url: path });
 			},
@@ -29,28 +29,14 @@ function AddButton(props) {
 			<View style={styles.view}>
 				<Icon {...styles.icon} />
 			</View>
-			{/* <WebView
-				source={{
-					uri: 'file:///android_asset/button.html',
-					baseUrl: 'file:///android_asset/'
-				}}
-				mixedContentMode="always"
-				originWhitelist={['*']}
-				allowFileAccess
-				allowUniversalAccessFromFileURLs
-				scrollEnabled={false}
-				onMessage={(e) => {
-				}}
-					console.log(JSON.parse(e.nativeEvent.data)['0']);
-				showsVerticalScrollIndicator={false}
-				showsHorizontalScrollIndicator={false}
-				style={{ flex: 1 }}
-			/> */}
 		</TouchableWithoutFeedback>
 	);
 }
 
-export default connect(null, actions)(AddButton);
+export default connect(
+	null,
+	actions
+)(AddButton);
 
 const styles = {
 	view: {
